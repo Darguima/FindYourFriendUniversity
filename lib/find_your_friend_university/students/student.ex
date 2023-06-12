62b1a9f76204549a0adec3d065d7ce2d2eeac778
Darguima <dsgdevbraga@gmail.com>
2023-06-12 17:12:21 +0100

    Created Students Entity

---
+++ lib/find_your_friend_university/students/student.ex
@@ -0,0 +1,21 @@
+defmodule FindYourFriendUniversity.Students.Student do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  # id = civil_id <> display_name
+  @primary_key {:id, :string, []}
+  schema "students" do
+    field(:civil_id, :string)
+    field(:display_name, :string)
+    field(:name, :string)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(student, attrs) do
+    student
+    |> cast(attrs, [:id, :name, :display_name, :civil_id])
+    |> validate_required([:id, :name, :display_name, :civil_id])
+  end
+end
