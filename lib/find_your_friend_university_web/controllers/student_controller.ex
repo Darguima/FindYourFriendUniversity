582fa1ef75b5c531d04e9e0b979b086f044fbaec
Darguima <dsgdevbraga@gmail.com>
2025-10-18 18:11:28 +0100

    Update code to 2025 applications

--- lib/find_your_friend_university_web/controllers/student_controller.ex
+++ lib/find_your_friend_university_web/controllers/student_controller.ex
@@ -32,7 +32,7 @@ defmodule FindYourFriendUniversityWeb.StudentController do
       )
       |> Map.update(
         "years_applications",
-        2018..2024 |> Enum.map(&Integer.to_string(&1)),
+        2018..2025 |> Enum.map(&Integer.to_string(&1)),
         fn array -> Enum.filter(array, fn elem -> elem != "" end) end
 
       )
